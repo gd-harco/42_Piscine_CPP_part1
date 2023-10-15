003ede3942c3348c9cb1228fc04d764a079db07b
gd-harco <gd-harco@student.42lyon.fr>
2023-10-15 14:55:33 +0200

    HumanA class Implemeted

--- Module_01/ex03/incs/HumanA.hpp
+++ Module_01/ex03/incs/HumanA.hpp
@@ -1,16 +1,31 @@
-//
-// Created by gd-harco on 14/10/23.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanA.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/15 14:55:15 by gd-harco          #+#    #+#             */
+/*   Updated: 2023/10/15 14:55:15 by gd-harco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef HUMANA_HPP
 #define HUMANA_HPP
+#include "Weapon.hpp"
+#include <iostream>
 
 
-
-class HumanA {
-
+class HumanA
+{
+public:
+	HumanA(std::string name, Weapon& toEquiped);
+	~HumanA();
+	void	attack();
+private:
+	Weapon&		_equiped;
+	std::string	_name;
 };
-
 
 
 #endif //HUMANA_HPP
--- Module_01/ex03/srcs/HumanA.cpp
+++ Module_01/ex03/srcs/HumanA.cpp
@@ -1,5 +1,27 @@
-//
-// Created by gd-harco on 14/10/23.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanA.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/15 14:55:23 by gd-harco          #+#    #+#             */
+/*   Updated: 2023/10/15 14:55:23 by gd-harco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "HumanA.hpp"
+
+HumanA::HumanA(std::string name,  Weapon& toEquiped):
+		_equiped(toEquiped), _name(name)
+{
+	std::cout << "HumanA " << this->_name << " created with Weapon" << this->_equiped.getType() << std::endl;
+}
+
+HumanA::~HumanA(){
+	std::cout << "HumanA " << this->_name << " deleted" << std::endl;
+}
+
+void	HumanA::attack(){
+	std::cout << this->_name << " attacked with their " << this->_equiped.getType() << std::endl;
+}
