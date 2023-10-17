7db49f4291a2e6be9559940a1bb43583d2447766
gd-harco <gd-harco@student.42lyon.fr>
2023-10-17 22:07:56 +0200

    solved ex04

--- Module_01/ex04/srcs/main.cpp
+++ Module_01/ex04/srcs/main.cpp
@@ -6,54 +6,60 @@
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 15:04:56 by gd-harco          #+#    #+#             */
-/*   Updated: 2023/10/16 16:43:15 by gd-harco         ###   ########.fr       */
+/*   Updated: 2023/10/17 21:36:25 by gd-harco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <sstream>
 
-//class Bonjour {
-//private:
-//	int	_i;
-//
-//public:
-//	Bonjour(int i): _i(i) {}
-//
-//	Bonjour&	operator+(const Bonjour&&)
-//};
-//
-//int main() {
-//	Bonjour bonjour(5);
-//	Bonjour bonsoir(10);
-//
-//	Bonjour bonneNuit = bonjour + bonsoir;
-//}
+static std::string    read_file(std::ifstream &file);
 
 int main(int argc, char **argv){
 	if (argc  != 4){
 		std::cout << "Wrong number of argument" << std::endl;
 		return (1);
 	}
-
-	std::string		fileInPath = argv[1];
 	std::string		fileInContent;
-	std::string		fileOutPath = fileInPath + ".replace";
+	std::string		fileOut = std::string(argv[1]) + ".replace";
 	std::string		toLook = argv[2];
 	std::string		toReplace = argv[3];
 	std::ifstream	input;
 	std::ofstream	output;
+	std::size_t		nextPos;
 
-	input.open(fileInPath.c_str());
-	if (input.fail())
+	input.open(argv[1]);
+	if (!input.is_open())
 	{
-		std::cout << "Error opening file " << fileInPath << std::endl;
+		std::cout << "Error opening file " << argv[1] << std::endl;
 		return (1);
 	}
-
-	input >> fileInContent;
+	fileInContent = read_file(input);
 	input.close();
-	output.open(fileOutPath.c_str());
-	output << fileInContent.substr(0, fileInContent.find(toLook));
+	output.open(fileOut.c_str());
+	if (!output.is_open())
+	{
+		std::cout << "Error opening file " << fileOut << std::endl;
+		return (1);
+	}
+	nextPos = fileInContent.find(toLook);
+	while (nextPos != std::string::npos)
+	{
+		output << fileInContent.substr(0, nextPos);
+		output << toReplace;
+		fileInContent = fileInContent.substr(nextPos + toLook.size(), fileInContent.size());
+		nextPos = fileInContent.find(toLook);
+	}
+	output << fileInContent;
+	output.close();
+	return (0);
+}
+
+std::string    read_file(std::ifstream &file) {
+	std::stringstream    file_content;
+
+	file_content << file.rdbuf();
+	return (file_content.str());
 }
