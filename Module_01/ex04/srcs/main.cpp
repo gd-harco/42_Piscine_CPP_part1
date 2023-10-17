1506d4dfb29083b282d7e97d74fdd39409dfa573
gd-harco <gd-harco@student.42lyon.fr>
2023-10-17 15:13:04 +0200

    progress

--- Module_01/ex04/srcs/main.cpp
+++ Module_01/ex04/srcs/main.cpp
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -14,15 +14,46 @@
 #include <string>
 #include <iostream>
 
+//class Bonjour {
+//private:
+//	int	_i;
+//
+//public:
+//	Bonjour(int i): _i(i) {}
+//
+//	Bonjour&	operator+(const Bonjour&&)
+//};
+//
+//int main() {
+//	Bonjour bonjour(5);
+//	Bonjour bonsoir(10);
+//
+//	Bonjour bonneNuit = bonjour + bonsoir;
+//}
+
 int main(int argc, char **argv){
 	if (argc  != 4){
 		std::cout << "Wrong number of argument" << std::endl;
 		return (1);
 	}
 
-	std::string file = argv[1];
-	std::string toLook = argv[2];
-	std::string toReplace = argv[3];
+	std::string		fileInPath = argv[1];
+	std::string		fileInContent;
+	std::string		fileOutPath = fileInPath + ".replace";
+	std::string		toLook = argv[2];
+	std::string		toReplace = argv[3];
+	std::ifstream	input;
+	std::ofstream	output;
 
+	input.open(fileInPath.c_str());
+	if (input.fail())
+	{
+		std::cout << "Error opening file " << fileInPath << std::endl;
+		return (1);
+	}
 
+	input >> fileInContent;
+	input.close();
+	output.open(fileOutPath.c_str());
+	output << fileInContent.substr(0, fileInContent.find(toLook));
 }
