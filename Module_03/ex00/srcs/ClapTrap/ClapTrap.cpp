fbfb96f21f7672ad18675d7ffa83d33820f48de3
gd-harco <gd-harco@student.42lyon.fr>
2023-10-24 16:57:56 +0200

    initial commit

---
+++ Module_03/ex00/srcs/ClapTrap/ClapTrap.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gd-harco on 24/10/23.
+//
+
+#include "ClapTrap.hpp"
---
+++ Module_03/ex00/srcs/ClapTrap/ClapTrap.hpp
@@ -0,0 +1,14 @@
+//
+// Created by gd-harco on 24/10/23.
+//
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+
+class ClapTrap {
+
+};
+
+
+#endif //CLAPTRAP_HPP
