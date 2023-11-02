c1f2bad3a9625ed9aaff954b727253a1c44fbe19
gd-harco <gd-harco@student.42lyon.fr>
2023-11-02 12:02:04 +0100

    Add DiamondTrap and update files
    This commit introduces the DiamondTrap class which extends the inherited classes FragTrap and ScavTrap. A field "_name" and a new constructor for initializing "name" were added for ScavTrap and FragTrap. Files which needed to link with these new classes were updated including related header and .cpp files. The changes were necessary for adding the specific functionality of the DiamondTrap. Along with this, .gitignore file has been added in the repository to avoid keeping unnecessary and subsidiary files. The code lines in other classes were also modified for better output representation.

---
+++ Module_03/ex03/srcs/DiamondTrap/DiamondTrap.cpp
@@ -0,0 +1,9 @@
+//
+// Created by gd-harco on 02/11/23.
+//
+
+#include "header.hpp"
+
+DiamondTrap::DiamondTrap(const std::string &name) : ClapTrap(name) {
+
+}
--- Module_03/ex03/srcs/DiamondTrap/DiamondTrap.hpp
+++ Module_03/ex03/srcs/DiamondTrap/DiamondTrap.hpp
@@ -1,28 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*   DiamondTrap.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/01 11:28:39 by gd-harco          #+#    #+#             */
-/*   Updated: 2023/11/02 11:53:30 by gd-harco         ###   ########.fr       */
+/*   Created: 2023/11/02 11:01:48 by gd-harco          #+#    #+#             */
+/*   Updated: 2023/11/02 11:40:19 by gd-harco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SCAVTRAP_HPP
-#define SCAVTRAP_HPP
-#include "ClapTrap.hpp"
 
-class ScavTrap : public ClapTrap {
+#ifndef DIAMONDTRAP_HPP
+#define DIAMONDTRAP_HPP
+
+#include "header.hpp"
+
+class DiamondTrap
+		: public FragTrap, public ScavTrap {
+private:
+	std::string _name;
 public:
-	ScavTrap();
-	ScavTrap(const std::string &name);
-	ScavTrap(const ScavTrap &old);
-	~ScavTrap();
-	void attack(const std::string target);
-	void guardGate();
+	DiamondTrap(const std::string &name);
 };
 
 
-#endif //SCAVTRAP_HPP
+#endif //DIAMONDTRAP_HPP
