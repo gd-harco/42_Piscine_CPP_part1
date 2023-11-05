144f2a1f1431e77ca30ee47b79456e0b4e1db16e
gd-harco <gd-harco@student.42lyon.fr>
2023-11-05 12:27:12 +0100

    solved ex06

--- Module_01/ex06/srcs/Harl.cpp
+++ Module_01/ex06/srcs/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 16:26:10 by gd-harco          #+#    #+#             */
-/*   Updated: 2023/10/19 21:17:38 by gd-harco         ###   ########.fr       */
+/*   Updated: 2023/11/05 12:25:07 by gd-harco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,46 @@
 #include "Harl.hpp"
 #include "header.hpp"
 
-void Harl::_debug(void){
+void Harl::_debug(void) {
 	std::cout << "This is a debug message." << std::endl;
+	this->_info();
 }
 
-void Harl::_info(void){
+void Harl::_info(void) {
 	std::cout << "This is an informative message." << std::endl;
+	this->_warning();
 }
 
-void Harl::_warning(void){
+void Harl::_warning(void) {
 	std::cout << "This is a warning message, you should stop and invistigate." << std::endl;
+	this->_error();
 }
 
-void Harl::_error(void){
+void Harl::_error(void) {
 	std::cout << "Segmentation fault :/\n(this is a message print by Harl, don't panic!)." << std::endl;
 }
 
-void Harl::complain(std::string level){
-	s_table table[4] = {
-		{"debug", &Harl::_debug},
-		{"info", &Harl::_info},
-		{"warning", &Harl::_warning},
-		{"error", &Harl::_error},
-	};
-	for (int i=0; i < 4; i++){
-		if (level.compare(table[i].level) == 0)
-			return (this->*table[i].call)();
+void Harl::complain(std::string level) {
+	int i = 0;
+	std::string table[4] = {"debug", "info", "warning", "error"};
+
+	while (i < 4 && level.compare(table[i]) != 0)
+		i++;
+	switch (i) {
+		case 0:
+			(Harl::_debug)();
+			break;
+		case 1:
+			(Harl::_info)();
+			break;
+		case 2:
+			(Harl::_warning)();
+			break;
+		case 3:
+			(Harl::_error)();
+			break;
+		default:
+			std::cout << "Probably complaining about insignificant problems" <<std::endl;
+			break;
 	}
-	std::cout << "Unrecognized level" << std::endl;
 }
--- Module_01/ex06/srcs/main.cpp
+++ Module_01/ex06/srcs/main.cpp
@@ -6,14 +6,14 @@
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 16:18:46 by gd-harco          #+#    #+#             */
-/*   Updated: 2023/10/19 20:22:07 by gd-harco         ###   ########.fr       */
+/*   Updated: 2023/11/05 12:25:25 by gd-harco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.hpp"
 
-int	main(int argc, char **argv){
-	if (argc != 2){
+int	main(int argc, char **argv) {
+	if (argc != 2) {
 		std::cout << "Usage: ./Harl level\nAvailable level : debug, info, warning or error" << std::endl;
 		return (1);
 	}
